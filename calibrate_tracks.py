e7c70fc3b8697e34f81de87e30090c99ecbc033f
Steven Eardley <steve@eardley.xyz>
2015-07-12 18:43:31 +0100

    calibrate tracks now runs as module, not script by default

--- calibrate_tracks.py
+++ calibrate_tracks.py
@@ -18,30 +18,31 @@ def get_char():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
 
-print "Calibrate the track speeds. The result will be a +/- offset for Motor1, the right track." \
-      "\nThe robot will start to move forwards, with equal motor speeds. Use the +/- keys until the" \
-      "robot drives in a straight line. Press q to end the program to stop the robot and print the offset."
+if __name__ == '__main__':
+    print "Calibrate the track speeds. The result will be a +/- offset for Motor1, the right track." \
+          "\nThe robot will start to move forwards, with equal motor speeds. Use the +/- keys until the" \
+          "robot drives in a straight line. Press q to end the program to stop the robot and print the offset."
 
-raw_input("\nPress enter key to continue, or ctrl-c to quit without testing the offset.")
+    raw_input("\nPress enter key to continue, or ctrl-c to quit without testing the offset.")
 
-pico = PicoBorgRev()
-pico.Init()
-pico.SetMotors(BASE_SPEED)
+    pico = PicoBorgRev()
+    pico.Init()
+    pico.SetMotors(BASE_SPEED)
 
-offset = float(0)
-while True:
-    try:
-        pico.SetMotor1(BASE_SPEED + offset)
-        direction = get_char()
-        if direction == '+' or direction == '=':
-            offset += OFFSET_INCREMENT
-        elif direction == '_' or direction == '-':
-            offset -= OFFSET_INCREMENT
-        elif direction == 'q':
-            raise KeyboardInterrupt
+    offset = float(0)
+    while True:
+        try:
+            pico.SetMotor1(BASE_SPEED + offset)
+            direction = get_char()
+            if direction == '+' or direction == '=':
+                offset += OFFSET_INCREMENT
+            elif direction == '_' or direction == '-':
+                offset -= OFFSET_INCREMENT
+            elif direction == 'q':
+                raise KeyboardInterrupt
 
-        print "{:+.2f}".format(offset)
-    except KeyboardInterrupt:
-        pico.SetMotors(0)
-        print "Done. The final offset is {:+.2f}".format(offset)
-        sys.exit(0)
+            print "{:+.2f}".format(offset)
+        except KeyboardInterrupt:
+            pico.SetMotors(0)
+            print "Done. The final offset is {:+.2f}".format(offset)
+            sys.exit(0)
